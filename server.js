0c045289bdfb6fdf9cc60507aeb00f59e4c23a49
Amine <amyn.hichri@gmail.com>
2018-01-11 12:43:46 +0100

    add comment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
+/*//vfdjkg
 
 // API file for interacting with MongoDB
 const api = require('./server/routes/api');
